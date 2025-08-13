4879431959aa6f35223465db5943cc146240519e
Amin <106281828+AminSaei@users.noreply.github.com>
2025-08-13 22:52:40 +0330

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function convert(){
     else if(tocelsius.checked){
         temp = Number(textbox.value);
         temp = (temp - 32) * (5/9);
-        result.textContent = temp.toFixed(1) + "°F";
+        result.textContent = temp.toFixed(1) + "°C";
     }
     else{
         result.textContent = "select a unit please!";
